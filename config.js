f9c530c488542e96ab0d839d7d0f1e1cdf92382a
Дүйсенбаев Нұрлан <d.nurlan.d@gmail.com>
2022-03-26 22:57:37 +0600

    изменен путь к базе

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   db: {
     name: "calendarDb",
-    url: "mongodb://localhost/"
-  }
+    // url: "mongodb://localhost/"
+    url: "mongodb://data.mongodb-api.com/app/data-fyvvo/endpoint/data/beta",
+  },
 };
