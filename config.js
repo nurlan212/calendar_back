ede3b957e8d0d79bf6a05a73fcd777e6fe79263b
Дүйсенбаев Нұрлан <d.nurlan.d@gmail.com>
2022-03-27 13:10:27 +0600

    исправлен url для db в mongodb atlas

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   db: {
     name: "calendarDb",
     // url: "mongodb://localhost/"
-    url: "mongodb://data.mongodb-api.com/app/data-fyvvo/endpoint/data/beta",
+    url: "mongodb+srv://dnurland:0211@cluster0.hy1ss.mongodb.net/",
   },
 };
